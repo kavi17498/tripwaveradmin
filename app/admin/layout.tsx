b458b8e0b89c42a962112dac77a58eab83206758
Kappanile Kavindu Lakshan Kottegoda <138689980+kavi17498@users.noreply.github.com>
2026-05-12 13:39:49 +0530

    removed top nav bar from the admin panel

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,11 +1,9 @@
 import { ReactNode } from "react";
-import { Navbar } from "@/components/layout/navbar";
 import { ProtectedRoute } from "@/components/wrappers/protected-route";
 
 export default function AdminLayout({ children }: { children: ReactNode }) {
   return (
     <ProtectedRoute allowRoles={["admin", "superadmin"]}>
-      <Navbar />
       <main className="mx-auto max-w-7xl p-4 md:p-6">{children}</main>
     </ProtectedRoute>
   );
